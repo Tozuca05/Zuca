6f28dbd71e060deb663eab76585e9d3cb5709b07
santig005 <sajhdg30@gmail.com>
2024-11-12 18:13:37 -0500

    feat: new values returned by the api

--- app/Http/Resources/ProductResource.php
+++ app/Http/Resources/ProductResource.php
@@ -13,8 +13,10 @@ class ProductResource extends JsonResource
             'id' => $this->id,
             'name' => $this->name,
             'price' => $this->price,
-            // Usa rtrim para eliminar cualquier barra al final de la URL base
+            'stock' => $this->stock,
             'link' => rtrim(config('app.url'), '/') . "/products/{$this->id}",
+            // Incluir el tag del producto
+            'tag' => $this->tag ? $this->tag->name : null, // Muestra el nombre del tag, si existe
         ];
     }
 }
